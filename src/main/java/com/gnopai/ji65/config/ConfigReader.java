e1342d5dee8937cd40ab1c08b885fe4d5207a22f
bennettm <mark.bennett@flightglobal.com>
2019-03-26 16:39:52 -0700

    Added config file interpreter-looking thing, and a top-level class that does the whole config scanning-parsing-interpreting process.

---
+++ src/main/java/com/gnopai/ji65/config/ConfigReader.java
@@ -0,0 +1,39 @@
+package com.gnopai.ji65.config;
+
+import com.gnopai.ji65.parser.TokenConsumer;
+import com.gnopai.ji65.scanner.Token;
+import com.gnopai.ji65.scanner.TokenReader;
+import com.gnopai.ji65.util.ErrorHandler;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ConfigReader {
+    private final ErrorHandler errorHandler;
+
+    public ConfigReader(ErrorHandler errorHandler) {
+        this.errorHandler = errorHandler;
+    }
+
+    public ProgramConfig read(String configText) {
+        return Optional.of(configText)
+                .map(this::scan)
+                .map(this::parse)
+                .map(this::interpret)
+                .orElseThrow();
+    }
+
+    private List<Token> scan(String configText) {
+        ConfigScanner configScanner = new ConfigScanner(new TokenReader(errorHandler));
+        return configScanner.scan(configText);
+    }
+
+    private List<ConfigBlock> parse(List<Token> tokens) {
+        ConfigParser configParser = new ConfigParser(new TokenConsumer(errorHandler, tokens));
+        return configParser.parse();
+    }
+
+    private ProgramConfig interpret(List<ConfigBlock> blocks) {
+        return new ConfigInterpreter().interpret(blocks);
+    }
+}
