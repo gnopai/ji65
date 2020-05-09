d6c494c65c8f9a4251f550f3c90b9e9eeb43e8e3
gnopai <vghype1@gmail.com>
2020-05-09 15:39:04 -0700

    Added functional tests for memory mocks and memory verifications.

--- src/test/java/com/gnopai/ji65/TestingFunctionalTest.java
+++ src/test/java/com/gnopai/ji65/TestingFunctionalTest.java
@@ -116,6 +116,86 @@ class TestingFunctionalTest {
     }
 
     @Test
+    void testMemoryMockTest() {
+        TestReport testReport = assembleAndRunTests(
+                "sub_to_test:",
+                "lda $2088",
+                "ldx $2088",
+                "ldy $2088",
+                "rts",
+                "",
+                ".test whee",
+                ".testmock $2088 $F4, $35, $90",
+                ".testrun sub_to_test",
+                ".assert A $F4",
+                ".assert X $35",
+                ".assert Y $90",
+                ".endtest"
+        );
+
+        TestReport expectedTestReport = new TestReport(List.of(
+                TestResult.builder()
+                        .name("whee")
+                        .assertionResult(new AssertionResult(true, (byte) 0xF4, (byte) 0xF4))
+                        .assertionResult(new AssertionResult(true, (byte) 0x35, (byte) 0x35))
+                        .assertionResult(new AssertionResult(true, (byte) 0x90, (byte) 0x90))
+                        .build()
+        ));
+        assertEquals(expectedTestReport, testReport);
+    }
+
+    @Test
+    void testMemoryVerifyTest_resultsPass() {
+        TestReport testReport = assembleAndRunTests(
+                "read_stuff:",
+                "lda $2001",
+                "rts",
+                "write_stuff:",
+                "sta $2002",
+                "rts",
+                "",
+                ".test whee",
+                ".testrun read_stuff",
+                ".testset A $F1",
+                ".testrun write_stuff",
+                ".testset A $52",
+                ".testrun write_stuff",
+                ".testrun read_stuff",
+                ".verifywrite $2002 $F1, $52",
+                ".verifyread $2001 2",
+                ".endtest"
+        );
+
+        TestReport expectedTestReport = new TestReport(List.of(
+                TestResult.builder()
+                        .name("whee")
+                        .assertionResult(new AssertionResult(true, List.of((byte) 0xF1, (byte) 0x52), List.of((byte) 0xF1, (byte) 0x52)))
+                        .assertionResult(new AssertionResult(true, 2, 2))
+                        .build()
+        ));
+        assertEquals(expectedTestReport, testReport);
+    }
+
+    @Test
+    void testMemoryVerifyTest_resultsFail() {
+        TestReport testReport = assembleAndRunTests(
+                ".test whee",
+                ".verifywrite $2002 $F1, $52",
+                ".verifyread $2001 2",
+                ".endtest"
+        );
+
+        TestReport expectedTestReport = new TestReport(List.of(
+                TestResult.builder()
+                        .name("whee")
+                        .assertionResult(new AssertionResult(false, List.of((byte) 0xF1, (byte) 0x52), List.of(), "Unexpected bytes written to address $2002"))
+                        .assertionResult(new AssertionResult(false, 2, 0, "Unexpected read count for address $2001"))
+                        .build()
+        ));
+        assertEquals(expectedTestReport, testReport);
+    }
+
+    @Test
     void testMultipleTestsWithMixedResults() {
         TestReport testReport = assembleAndRunTests(
                 "sub_to_test:",
